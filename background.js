bfab62a73516c5b8c19a17c9dbfac2afa10fda89
kaitlynnchan <kaitlynnchan@outlook.com>
2021-11-04 00:08:34 -0700

    Implement chrome extension
    Send tab url to content.js from popup.js to background.js

---
+++ background.js
@@ -0,0 +1,24 @@
+console.log("background running");
+
+/**
+ * sending messages from background to content
+ * use browser action
+ */
+
+// requirement: no popup.html
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//     let url = tab.url;
+//     console.log("browser clicked");
+//     chrome.tabs.sendMessage(tab.id, url);
+// });
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse){
+    if(message.action == 'run_background'){
+        // sends information of tabs to content
+        chrome.tabs.query({active: true}, function(tabs){
+            console.log(tabs)
+            chrome.tabs.sendMessage(tabs[0].id, {action: "run_content", url: tabs[0].url});  
+        });
+    }
+    console.log("background received message");
+});
---
+++ content.js
@@ -0,0 +1,11 @@
+console.log('Extension is activated');
+
+/**
+ *  receiving messages
+ */
+chrome.runtime.onMessage.addListener( function(message, sender, sendResponse){
+    if(message.action == 'run_content'){
+        console.log(message.url);
+    }
+    console.log("content received message");
+});
