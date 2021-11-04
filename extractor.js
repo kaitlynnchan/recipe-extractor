2336419ea46eabeba0f0fd219f06c9deaba7c773
kaitlynnchan <kaitlynnchan@outlook.com>
2021-11-04 09:11:01 -0700

    Add jquery

--- extractor.js
+++ extractor.js
@@ -1,16 +1,33 @@
 /** Following tutorial: https://www.linkedin.com/pulse/scraping-recipe-websites-ben-awad */
 
-import fetch from 'node-fetch';
+console.log("extractor running");
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse){
+    if(message.action == 'run_extractor'){
+        // gets tab url from background
+        console.log(message.url);
+        $("script").load(message.url);
+        console.log("extractor received message from background");
+    }
+});
 
-function extractHtml(html){
-    var tempHtml = document.createElement("div");
-    tempHtml.innerHTML = html;
-    var jsonld = tempHtml.getElementsByTagName("script");
-    return jsonld;
-}
+// $(document).ready(function(){
+    
+// });
+
+// async function getHtmlText(url){
+//     const html = await fetch(url).then(x => x.text());
+//     return html;
+// }
+
+// function extractHtml(html){
+//     var tempHtml = document.createElement("div");
+//     tempHtml.innerHTML = html;
+//     var jsonld = tempHtml.getElementsByTagName("script");
+//     return jsonld;
+// }
 
 
-var url = "https://www.seasonsandsuppers.ca/soft-cheese-bread-recipe/#wprm-recipe-container-20224";
-const html = await fetch(url).then(x => x.text());
+// var url = "https://www.seasonsandsuppers.ca/soft-cheese-bread-recipe/#wprm-recipe-container-20224";
+// const html = await fetch(url).then(x => x.text());
 
-console.log(extractHtml(html));
+// console.log(extractHtml(html));
--- popup.js
+++ popup.js
@@ -5,9 +5,17 @@
 
 // run background script
 window.onload = function(){
-  var calculateBtn = document.getElementsByClassName("calculate");
-  calculateBtn[0].addEventListener("click", function(){
-    console.log("clicked");
-    chrome.runtime.sendMessage({action:"run_background"});
-  });
+    var calculateBtn = document.getElementsByClassName("calculate");
+    calculateBtn[0].addEventListener("click", function(){
+        console.log("clicked");
+
+        // popup send message to background to run
+        chrome.runtime.sendMessage({action:"run_background"});
+        
+        // chrome.tabs.query({active: true}, function(tabs){
+        //     console.log(tabs[0].url);
+        //     var html = getHtmlText(tabs[0].url);
+        //     console.log(html);
+        // });
+    });
 };
