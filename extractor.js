27f0f7f8defad6ff650d9cf44133d5a2962aa2b3
kaitlynnchan <kaitlynnchan@outlook.com>
2021-11-03 21:03:58 -0700

    Add extract html function.
    Convert text to html.

--- extractor.js
+++ extractor.js
@@ -2,7 +2,15 @@
 
 import fetch from 'node-fetch';
 
+function extractHtml(html){
+    var tempHtml = document.createElement("div");
+    tempHtml.innerHTML = html;
+    var jsonld = tempHtml.getElementsByTagName("script");
+    return jsonld;
+}
+
+
 var url = "https://www.seasonsandsuppers.ca/soft-cheese-bread-recipe/#wprm-recipe-container-20224";
 const html = await fetch(url).then(x => x.text());
 
-console.log(html);
+console.log(extractHtml(html));
